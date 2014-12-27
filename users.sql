69b9c88922750d0060d8ab9bc41b32287beed9d3
Ilan Kleiman <ilankleiman@gmail.com>
2014-12-27 03:05:42 -0500

    SQL Database example template

--- users.sql
+++ users.sql
@@ -1,30 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 4.0.10.6
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Dec 20, 2014 at 10:19 PM
--- Server version: 5.5.36-cll-lve
--- PHP Version: 5.4.23
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `database`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 
 CREATE TABLE IF NOT EXISTS `users` (
   `ID` mediumint(9) NOT NULL AUTO_INCREMENT,
@@ -38,14 +13,3 @@ CREATE TABLE IF NOT EXISTS `users` (
   `cookie_set` text NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`ID`, `real_user`, `username`, `password`, `salt`, `email`, `u_cookie`, `p_cookie`, `cookie_set`) VALUES
-(1, 'username', 'encoded_username', 'encrypted_password', 'salt', 'email', 'ucookie', 'pcookie', 'date cookie was set');
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
